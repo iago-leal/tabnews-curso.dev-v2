bf029571493e4a8ba45b89262d5904dd80b4cedc
Iago Leal <iagolealdev@gmail.com>
2026-08-10 22:12:13 +0000

    atualiza .env para integracao com db remoto

--- infra/database.js
+++ infra/database.js
@@ -3,14 +3,14 @@ import { Client } from "pg";
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: process.env.POSTGRES_URL,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   console.log("Connecting to database...", {
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
+    port: process.env.POSTGRES_URL,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,

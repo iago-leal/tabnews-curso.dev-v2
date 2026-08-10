e6bb398d8bc216d1822ac131dc5da68e7a1101fe
Iago Leal <iagolealdev@gmail.com>
2026-08-10 22:06:44 +0000

    atualiza .env para integracao com db remoto

--- infra/database.js
+++ infra/database.js
@@ -5,14 +5,14 @@ async function query(queryObject) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   console.log("Connecting to database...", {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
+    database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
   });
   try {
